b5fa52dbadda9c9c24db65856d7055c8932c65f9
Michael Temblador <michaeltemblador@Michaels-iMac.local>
2021-03-20 17:22:49 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 var timeContainer = document.querySelector(".lead")
+var list = JSON.parse(localStorage.getItem('form')) || [];
 timeContainer.innerHTML = currentTime;
+
+$ ("row").each(function(i) {
+    if(this < currentTime) {
+        this.style.color = "grey"
+    }
+})
 
 $("button").click(function(){
     var toDoTask = $('.form')
